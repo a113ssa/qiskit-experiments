3a742682b035b8a5e64c120cc6a1b0b91899c883
a113ssa <alessa.silent.hill.insider@gmail.com>
2025-02-03 22:10:14 +0400

    Add Hadamard gate demo

--- custom_utils.py
+++ custom_utils.py
@@ -8,8 +8,10 @@ def create_circuit(qubits=1):
     circuit.rz(0, qubits_arr)
     return circuit
 
-def run_simulation(circ):
-    simulator = AerSimulator(method='statevector')
-    circ.save_statevector()
+def run_simulation(circ, method='statevector'):
+    simulator = AerSimulator(method=method)
+    if method == 'statevector':
+        circ.save_statevector()
+
     result = simulator.run(circ).result()
     return result
