ed3c8477545d8ae528a044c8780013a6703f7dda
a113ssa <alessa.silent.hill.insider@gmail.com>
2025-02-03 16:44:26 +0400

    Add Pauli Y Gate demo

--- custom_utils.py
+++ custom_utils.py
@@ -1,12 +1,10 @@
-from math import pi
-
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
 
 
 def create_circuit(qubits=1):
     circuit = QuantumCircuit(qubits)
-    circuit.ry(pi/4, 0)
+    circuit.rz(0, 0)
     return circuit
 
 def run_simulation(circ):
